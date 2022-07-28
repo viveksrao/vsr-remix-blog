0188df416b95d9afbcedd65a41d10e3a344ea422
Vivekanand Rao <vivekanand.rao@gmail.com>
2022-07-28 11:26:00 -0700

    Model Data in the Prisma Schema for a Remix App

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,18 +1,5 @@
-type Post = {
-  slug: string;
-  title: string;
-};
+import { prisma } from "~/db.server";
 
 export async function getPosts() {
-  const posts = [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
-  return posts;
+  return prisma.post.findMany();
 }
