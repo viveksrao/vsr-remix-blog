3fb1d1efc406e3632f5e300e3938cdc49e70eac2
Vivekanand Rao <vivekanand.rao@gmail.com>
2022-07-28 12:34:32 -0700

    Typing Form Submission Values with tiny-invariant

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -2,6 +2,15 @@ import { Form, useActionData } from "@remix-run/react";
 import { json, redirect } from "@remix-run/node";
 import type { ActionFunction } from "@remix-run/node";
 import { createPost } from "~/models/post.server";
+import invariant from "tiny-invariant";
+
+type ActionData =
+  | {
+      title: null | string;
+      slug: null | string;
+      markdown: null | string;
+    }
+  | undefined;
 
 export const action: ActionFunction = async ({ request, params }) => {
   const formData = await request.formData();
@@ -18,8 +27,12 @@ export const action: ActionFunction = async ({ request, params }) => {
 
   const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
   if (hasErrors) {
-    return json(errors);
+    return json<ActionData>(errors);
   }
+
+  invariant(typeof title === "string", "title must be a string");
+  invariant(typeof slug === "string", "slug must be a string");
+  invariant(typeof markdown === "string", "markdown must be a string");
 
   await createPost({ title, slug, markdown });
   return redirect("/posts/admin");
@@ -28,7 +41,7 @@ export const action: ActionFunction = async ({ request, params }) => {
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
 export default function NewPostRoute() {
-  const errors = useActionData();
+  const errors = useActionData() as ActionData;
   return (
     <Form method="post">
       <p>
